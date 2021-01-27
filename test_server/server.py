201ac25735522f9a3d61c5e23058b133a5a8f621
Malhar Vora <mlvora.2010@gmail.com>
2021-01-27 06:17:04 -0800

    Add POST route to test python server

--- test_server/server.py
+++ test_server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 IP = "0.0.0.0"
 PORT = "15000"
@@ -6,9 +6,15 @@ PORT = "15000"
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/gettest')
 def route1():
     print("route1 called")
+    return 'Hello, World!'
+
+@app.route('/posttest', methods=["POST"])
+def route2():
+    print("route2 called")
+    print(request.json)
     return 'Hello, World!'
 
 app.run(host=IP, port=PORT)
