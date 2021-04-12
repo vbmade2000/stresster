4a03e021dd7c613820391a1bc2a9e7ea210ae219
Malhar Vora <mlvora.2010@gmail.com>
2021-04-12 09:25:50 -0700

    Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use enums::{Command, HTTPMethods};
 use futures::future::join_all;
 use output_producers::{json_producer, table_producer};
 use reqwest::{
-    header::{HeaderMap, HeaderName, HeaderValue},
+    header::{HeaderName, HeaderValue},
     Client,
 };
 use serde_json::Value;
@@ -52,90 +52,60 @@ async fn counting_machine(counter_map: COUNTERMAP, mut rx: tokio::sync::mpsc::Re
     }
 }
 
-async fn send(sender: tokio::sync::mpsc::Sender<Command>, logger: LOGGER, data: Option<DATA>) {
+async fn send(sender: tokio::sync::mpsc::Sender<Command>, logger: LOGGER, data: DATA) {
     // Common vars
-    // -> let target_url = url.clone();
     let client = Client::new();
     let result; // For storing request result
     let logger = logger.clone();
 
-    /* A default value for payload in case any of them is not supplied. We can put a condition
-       to check if they are supplied but that doesn't feel idiomatic way.
-    */
-    let default_payload: serde_json::Value = serde_json::from_str("{}").unwrap();
+    let data = data.clone();
+    let payload = &data.payload;
+    let headers = data.headers.clone();
+    let method = data.method.clone();
+    let target_url = data.url.to_string();
 
-    let mut a_payload = &default_payload;
-    let mut a_headers = HeaderMap::new();
-    let mut method = HTTPMethods::fromstr("GET".to_string()).unwrap();
-    let adata: std::sync::Arc<data::Data>;
-    let mut target_url = "NA".to_string();
-
-    if data.is_some() {
-        adata = data.clone().unwrap();
-        a_payload = &adata.payload;
-        a_headers = adata.headers.clone();
-        method = adata.method.clone();
-        target_url = adata.url.to_string();
-    }
-
+    info!(
+        logger,
+        "Sending {:?} request to {:?} with payload {:?}", method, target_url, payload
+    );
     match method {
         HTTPMethods::GET => {
-            info!(
-                logger,
-                "Sending {:?} request to {:?} with payload {:?}", method, target_url, a_payload
-            );
             result = client
                 .get(&*target_url)
-                .json(a_payload)
-                .headers(a_headers.clone())
+                .json(payload)
+                .headers(headers.clone())
                 .send()
                 .await;
         }
         HTTPMethods::POST => {
-            info!(
-                logger,
-                "Sending {:?} request to {:?} with payload {:?}", method, target_url, a_payload
-            );
             result = client
                 .post(&*target_url)
-                .json(a_payload)
-                .headers(a_headers.clone())
+                .json(payload)
+                .headers(headers.clone())
                 .send()
                 .await;
         }
         HTTPMethods::PUT => {
-            info!(
-                logger,
-                "Sending {:?} request to {:?} with payload {:?}", method, target_url, a_payload
-            );
             result = client
                 .put(&*target_url)
-                .json(a_payload)
-                .headers(a_headers.clone())
+                .json(payload)
+                .headers(headers.clone())
                 .send()
                 .await;
         }
         HTTPMethods::DELETE => {
-            info!(
-                logger,
-                "Sending {:?} request to {:?} with payload {:?}", method, target_url, a_payload
-            );
             result = client
                 .delete(&*target_url)
-                .json(a_payload)
-                .headers(a_headers.clone())
+                .json(payload)
+                .headers(headers.clone())
                 .send()
                 .await;
         }
         HTTPMethods::PATCH => {
-            info!(
-                logger,
-                "Sending {:?} request to {:?} with payload {:?}", method, target_url, a_payload
-            );
             result = client
                 .patch(&*target_url)
-                .json(a_payload)
-                .headers(a_headers.clone())
+                .json(payload)
+                .headers(headers.clone())
                 .send()
                 .await;
         }
@@ -158,14 +128,14 @@ async fn send(sender: tokio::sync::mpsc::Sender<Command>, logger: LOGGER, data: 
 async fn main() {
     // Prepare for argument parsing
     let matches = App::new("stresster")
-        .version("1.0")
+        .version("0.1.0")
         .author("Malhar Vora <vbmade2000@gmail.com>")
         .arg(
-            Arg::with_name("payload")
-                .short("p")
-                .long("payload")
-                .value_name("payload")
-                .help("File containing payload to send")
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("config")
+                .help("Configuration file containing data to send")
                 .takes_value(true)
                 .required(true),
         )
@@ -190,80 +160,79 @@ async fn main() {
 
     // Extract user supplied values
     let output_format = matches.value_of("format").unwrap();
-    let payload_filename = matches.value_of("payload").unwrap().to_string();
+    let config_filename = matches.value_of("config").unwrap().to_string();
     let total_requests: i32 = matches.value_of("requests").unwrap().parse().unwrap();
 
-    let mut shared_data: Option<DATA> = None; // Data shared between tasks
-    let mut content: Option<Value> = None; // Stores JSON data
+    let shared_data: DATA; // Data shared between tasks
+    let content: Value; // Stores JSON data read from file
 
-    // Process payload only if filename supplied
-    if !payload_filename.is_empty() {
-        let result = fs::read_to_string(payload_filename.to_string());
-        match result {
-            Ok(r) => {
-                let result = serde_json::from_str(&r);
-                match result {
-                    Ok(p) => content = Some(p),
-                    Err(e) => {
-                        println!("ERROR: {}", e);
-                        exit(1)
-                    }
+    // TODO: Make error handling compact
+    let result = fs::read_to_string(config_filename.to_string());
+    match result {
+        Ok(r) => {
+            let result = serde_json::from_str(&r);
+            match result {
+                Ok(p) => content = p,
+                Err(e) => {
+                    println!("ERROR: {}", e);
+                    exit(1)
                 }
             }
-            Err(e) => {
-                println!("ERROR: {}, {}", payload_filename, e);
-                exit(1)
-            }
+        }
+        Err(e) => {
+            println!("ERROR: {}, {}", config_filename, e);
+            exit(1)
         }
     };
 
+    // Default values in case actual values are not supplied
     let default_payload: serde_json::Value = serde_json::from_str("{}").unwrap();
     let default_headers: serde_json::Value = serde_json::from_str("{}").unwrap();
 
-    // We share payload only if we have something to share
-    if content.is_some() {
-        let unwrapped_content = content.unwrap();
+    // Extract payload if supplied
+    let actual_payload = content.get("payload").unwrap_or(&default_payload);
+    let mut data: Data = Data::default();
+    data.payload = actual_payload.clone();
 
-        // Extract payload if supplied
-        let actual_payload = unwrapped_content.get("payload").unwrap_or(&default_payload);
-        let mut data: Data = Data::default();
-        data.payload = actual_payload.clone();
+    // Extract HTTP headers
+    let headers = content
+        .get("headers")
+        .unwrap_or(&default_headers)
+        .as_object()
+        .unwrap();
 
-        // Extract HTTP headers
-        let headers = unwrapped_content
-            .get("headers")
-            .unwrap_or(&default_headers)
-            .as_object()
-            .unwrap();
-
-        // Insert extracted headers to shared object
-        for (key, value) in headers {
-            data.headers.insert(
-                HeaderName::from_lowercase(key.to_lowercase().as_bytes()).unwrap(),
-                HeaderValue::from_str(value.as_str().unwrap()).unwrap(),
-            );
-        }
-
-        let method = unwrapped_content
-            .get("method")
-            .expect("ERROR: Please specify method in payload file")
-            .as_str()
-            .unwrap()
-            .to_string();
-        let http_method = HTTPMethods::fromstr(method.to_uppercase());
-        if !http_method.is_some() {
-            println!("ERROR: Invalid HTTP method {:?}", method);
-            exit(1);
-        }
-        data.method = http_method.unwrap();
-        data.url = unwrapped_content
-            .get("url")
-            .expect("ERROR: Please specify URL in payload file")
-            .as_str()
-            .unwrap()
-            .to_string();
-        shared_data = Some(Arc::new(data));
+    // Insert extracted headers to shared object
+    for (key, value) in headers {
+        data.headers.insert(
+            HeaderName::from_lowercase(key.to_lowercase().as_bytes()).unwrap(),
+            HeaderValue::from_str(value.as_str().unwrap()).unwrap(),
+        );
     }
+
+    // Extract HTTP method
+    let method = content
+        .get("method")
+        .expect("ERROR: Please specify method in payload file")
+        .as_str()
+        .unwrap()
+        .to_string();
+
+    // Grab enum value from string HTTP method
+    let http_method = HTTPMethods::fromstr(method.to_uppercase());
+    if !http_method.is_some() {
+        println!("ERROR: Invalid HTTP method {:?}", method);
+        exit(1);
+    }
+    data.method = http_method.unwrap();
+
+    // Extract URL
+    data.url = content
+        .get("url")
+        .expect("ERROR: Please specify URL in payload file")
+        .as_str()
+        .unwrap()
+        .to_string();
+    shared_data = Arc::new(data);
 
     // Variables shared between tasks
     let counter = Arc::new(Mutex::new(HashMap::new())); // Map of Atomic counters to keep HTTP status code count
