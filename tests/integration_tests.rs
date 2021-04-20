d2654aaf4bcab83f19f83f61b1025e7a3b9cafd2
Malhar Vora <mlvora.2010@gmail.com>
2021-04-19 17:01:08 -0700

    Add Integration test to check if header is supplied properly

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,9 +1,13 @@
-use serde_json::Value;
+use serde_json::{from_str, json, Value};
+use std::str;
 use std::{
-    env, fs,
+    env,
+    env::temp_dir,
+    fs,
     path::PathBuf,
     process::{Command, Stdio},
 };
+use uuid::Uuid;
 
 const STRESSTER_PATH: &str = "STRESSTER_PATH";
 const DATA_FILE_PATH: &str = "DATA_FILE_PATH";
@@ -32,10 +36,6 @@ fn log_file_exists() {
         .output()
         .expect("ERROR: Error in executing stresster binary");
 
-    // Grab the output
-    // let val: Value =
-    //   serde_json::from_str(&String::from_utf8_lossy(&stresster_output.stdout)).unwrap();
-
     // Log file must be created
     assert_eq!(log_file_path.exists(), true);
 
@@ -43,9 +43,72 @@ fn log_file_exists() {
     assert_eq!(fs::metadata(log_file_path).unwrap().len() > 0, true)
 }
 
+/// Tests if headers from payload file are sent successfully
+#[test]
+fn test_headers() {
+    // Get paths from env vars
+    let stresster_path = get_path_from_env_var(STRESSTER_PATH.to_string());
+
+    /* We need to create a new data file from original file. It will contain an
+     * extra header we want to supply. This way we keep original file intact.
+     * We create new file with a UUID as a name to keep it unique and also in new
+     * temporary directry to avoid conflicts.
+     */
+    let dir = temp_dir();
+    let mut temp_file_name = PathBuf::new();
+    temp_file_name.push(dir);
+    temp_file_name.push(format!("{}.json", Uuid::new_v4().to_string()));
+
+    // Read JSON from file
+    // let mut file = fs::File::open(temp_file_name).unwrap();
+    let data = json!({
+        "url": "http://localhost:15000/gettest",
+        "ssl_cert": "./test_server/cert.pem",
+        "method": "get",
+        "payload": {
+            "name": "Malhar Vora"
+        },
+        "headers": {
+            "User-Agent": "stresster",
+            "Content-Type": "application/json",
+            "code": "204"
+        }
+    });
+
+    //file.write_all(data.as_str().unwrap());
+    let _ = serde_json::to_writer(
+        &fs::File::create(temp_file_name.to_string_lossy().to_string()).expect(
+            format!(
+                "Unable to create temporary data file {}",
+                temp_file_name.to_string_lossy()
+            )
+            .as_str(),
+        ),
+        &data,
+    );
+
+    // Execute stresster
+    let output = Command::new(stresster_path)
+        .arg("--config")
+        .arg(temp_file_name.to_str().unwrap())
+        .arg("--requests")
+        .arg("1")
+        .arg("--format")
+        .arg("json")
+        .stdout(Stdio::piped())
+        .output()
+        .expect("ERROR: Error in executing stresster binary");
+
+    // Extract total failed request from output
+    let output: Value = from_str(str::from_utf8(&output.stdout).unwrap())
+        .expect("Unable to convert stresster output to JSON");
+    let total_failed_requests = output.get("0").unwrap();
+    assert_eq!(total_failed_requests, 1);
+}
+
 /// Convert String in JSON format to Value
 fn _get_value_from_json(json: String) -> Value {
-    serde_json::from_str(&json).unwrap()
+    from_str(&json).unwrap()
 }
 
 /// Get PathBuf from path stored in ev var
